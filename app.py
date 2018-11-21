bd16d8bff3b2311a0ddf4295fc88baba54f90150
JeonChangMin <wisechang1@naver.com>
2018-11-21 11:58:48 +0900

    2018.11.21.class: Estimation file saving... - ChangMin

--- app.py
+++ app.py
@@ -7,34 +7,33 @@ import estimation__, petition__, job__, reservation__
 def index():
     return render_template("index.html")
 
-@app.route("intro")         # 소개 - 카페 풍경
+@app.route("/intro")         # 소개 - 카페 풍경
 def intro():
     return render_template("intro.html")
 
-@app.route("menu")          # 메뉴
+@app.route("/menu")          # 메뉴
 def menu():
     return render_template("menu.html")
 
-@app.route("way")           # 찾아오는길
+@app.route("/way")           # 찾아오는길
 def way():
     return render_template("way.html")
 
-@app.route("estimation")    # 평가(후기)
+@app.route("/estimation")    # 평가(후기)
 def estimation():
     return render_template("estimation.html")
 
-@app.route("petition")      # 청원
+@app.route("/petition")      # 청원
 def petition():
     return render_template("petition.html")
 
-@app.route("job")           # 구인구직
+@app.route("/job")           # 구인구직
 def job():
     return render_template("job.html")
 
-@app.route("reservation")   # 예약
+@app.route("/reservation")   # 예약
 def reservation():
     return render_template("reservation.html")
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
-    
--- estimation__.py
+++ estimation__.py
@@ -1,1 +1,15 @@
 # 평가
+import os
+
+class Estimation:
+    def __init__(self):
+        EstimationData = {}
+    def SaveEstimation(self, name, time, passwd, content):
+        f = open(name + time, 'r')
+        f.write(name+time+"\n"+passwd+"\n"+content)
+        return
+    def ChangeEstimation(self, name, time, passwd, content):
+        f = open(name + time, 'r')
+        f.write(name+time+"\n"+passwd+"\n"+content)
+    def CheckPassword(self, name, time, passwd):
+        f = open(name + time, 'w')
