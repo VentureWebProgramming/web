c8e4e0591f6a3a2a290d9a921240b08fcf2ba7d5
JeonChangMin <wisechang1@naver.com>
2018-12-09 13:26:00 +0900

    adding index..

--- app.py
+++ app.py
@@ -1,77 +1,39 @@
 from flask import Flask, render_template, request, Response
-import json
+import json, os, math
 app = Flask(__name__)
 
 from data_control import DataControl
 
-Est = DataControl("estimation")
-Job = DataControl("job")
-Pet = DataControl("petition")
-Rev = DataControl("reservation")
+listNum = 5
 
-# HOME--------------------------------------------------
+Data = {}
+Data["estimation"] = DataControl("estimation", listNum)
+Data["job"] = DataControl("job", listNum)
+Data["petition"] = DataControl("petition", listNum)
+Data["reservation"] = DataControl("reservation", listNum)
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Intro 소개---------------------------------------------
-@app.route("/intro")
-def intro():
-    return render_template("intro.html")
+@app.route("/<kind>")
+def rendering(kind):
+    return render_template(kind+".html", length=math.ceil(len(os.listdir("./data/"+kind))/listNum))
 
-# Menu 메뉴-----------------------------------------------
-@app.route("/menu")
-def menu():
-    return render_template("menu.html")
+@app.route("/<kind>/write", methods=['POST'])
+def write(kind):
+    data = json.loads(request.data)
+    return Data[kind].saveData(data)
 
-# Way 찾아오는길-------------------------------------------
-@app.route("/way")
-def way():
-    return render_template("way.html")
+@app.route("/<kind>/rewrite", methods=['POST'])
+def rewrite(kind):
+    print("Hello")
+    data = json.loads(request.data)
+    return Data[kind].changeData(data)
 
-# Estimation 평가------------------------------------------
-@app.route("/estimation")
-def estimation():
-    return render_template("estimation.html")
-
-@app.route("/estimation/write", methods=['POST'])
-def DoEstimation():
-    estimate_data = json.loads(request.data)
-    return Est.saveData(estimate_data)
-
-# Petition 청원------------------------------------------
-@app.route("/petition")
-def petition():
-    return render_template("petition.html")
-
-@app.route("/petition/write", methods=['POST'])
-def DoPetition():
-    petition_data = json.loads(request.data)
-    return Pet.saveData(petition_data)
-
-# Job 구인구직--------------------------------------------
-@app.route("/job")
-def job():
-    return render_template("job.html")
-
-@app.route("/job/write", methods=['POST'])
-def ApplyJob():
-    job_data = json.loads(request.data)
-    return Job.saveData(job_data)
-
-# Reservation 예약--------------------------------------------
-@app.route("/reservation")
-def reservation():
-    return render_template("reservation.html")
-
-@app.route("/reservation/reservation", methods=['POST'])
-def DoReservation():
-    reserve_data = json.loads(request.data)
-    return Rev.saveData(reserve_data)
-
-@app.route("/reservation/data", methods=['GET'])
-def getAllData():
-    return Response(response=Rev.getAllData(), status=200, mimetype='text/plain')
+@app.route("/<kind>/data/<idx>")
+def getData(kind, idx):
+    return Response(response=Data[kind].getData(idx), status=200, mimetype='text/plain')
 
 # Server Start
 if __name__ == "__main__":
--- data_control.py
+++ data_control.py
@@ -1,11 +1,11 @@
-# 예약
-import json, os
+import json, os, math
 
 class DataControl:
-    def __init__(self, path):
+    def __init__(self, path, listNum):
         self.directory = "./data/" + path
         self.successRet = '{"success": "True"}'
         self.failedRet = '{"success": "False"}'
+        self.listNum = listNum
 
     # arg 시간, 닉네임 -> 저장된 json 파일경로 return
     def getFilenameStr(self, now, name):
@@ -24,11 +24,11 @@ class DataControl:
             return self.failedRet
     
     # 모든 정보 불러오기
-    def getAllData(self):
-        print(os.listdir(self.directory))
+    def getData(self, idx):
         ret = "["
+        if math.ceil(len(os.listdir(self.directory))/self.listNum) < idx:
+            return self.failedRet
         for filename in os.listdir(self.directory):
-            print(filename)
             try:
                 f = open(self.directory+"/"+filename, 'r')
                 ret += f.read()+","
@@ -36,8 +36,6 @@ class DataControl:
             except IOError:
                 return self.failedRet
         ret = ret[0:len(ret)-1] + "]"
-        print(type(ret))
-        print(ret)
         return ret
 
     # 비밀번호 확인
@@ -46,7 +44,7 @@ class DataControl:
             f = open(self.getFilenameStr(now, name), 'r')
             obj = json.loads(f.read())
             f.close()
-            if obj["passwd"] == passwd:
+            if obj["password"] == passwd:
                 return True
             else:
                 return False
@@ -54,9 +52,9 @@ class DataControl:
             return False
     
     # 데이터 수정
-    def changeData(self, pnow, pname, ppasswd, data):
-        if self.passwdCheck(pnow, pname, ppasswd):
-            filename = self.getFilenameStr(pnow, pname)
+    def changeData(self, data):
+        if self.passwdCheck(data["now"], data["name"], data["password"]):
+            filename = self.getFilenameStr(data["now"], data["name"])
             if os.path.isfile(filename):
                 os.remove(filename)
             else:
