2b941818157640400b513a9009a1b0a40d82e351
JeonChangMin <wisechang1@naver.com>
2018-12-10 21:42:12 +0900

    ASDFASD

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ app = Flask(__name__)
 
 from data_control import DataControl
 
-listNum = 5
-
 Data = {}
-Data["review"] = DataControl("review", listNum)
-Data["recruit"] = DataControl("recruit", listNum)
-Data["petition"] = DataControl("petition", listNum)
-Data["reservation"] = DataControl("reservation", listNum)
+Data["review"] = DataControl("review")
+Data["recruit"] = DataControl("recruit")
+Data["petition"] = DataControl("petition")
+Data["reservation"] = DataControl("reservation")
 
 @app.route("/")
 def index():
@@ -19,26 +17,23 @@ def index():
 @app.route("/<kind>")
 def rendering(kind):
     print(kind)
-    if os.path.exists("./data/"+kind+"/"):
-        return render_template(kind+".html", length=math.ceil(len(os.listdir("./data/"+kind))/listNum))
+    if kind == "review":
+        return render_template("review.html")
     return render_template(kind+".html")
 
 @app.route("/<kind>/write", methods=['POST'])
 def write(kind):
-    print("write")
     data = json.loads(request.data)
     return Data[kind].saveData(data)
 
 @app.route("/<kind>/rewrite", methods=['POST'])
 def rewrite(kind):
-    print("rewrite")
     data = json.loads(request.data)
     return Data[kind].changeData(data)
 
-@app.route("/<kind>/data/<idx>")
-def getData(kind, idx):
-    print("data")
-    return Response(response=Data[kind].getData(idx), status=200, mimetype='text/plain')
+@app.route("/review/data")
+def getData(kind):
+    return Response(response=Data[kind].getData(), status=200, mimetype='text/plain')
 
 # Server Start
 if __name__ == "__main__":
--- data_control.py
+++ data_control.py
@@ -1,12 +1,11 @@
 import json, os, math
 
 class DataControl:
-    def __init__(self, kind, listNum):
+    def __init__(self, kind):
         self.kind = kind
         self.directory = "./data/" + kind
         self.successRet = '{"success": true}'
         self.failedRet = '{"success": false}'
-        self.listNum = listNum
 
     # arg 시간, 닉네임 -> 저장된 json 파일경로 return
     def getFilenameStr(self, now, name):
@@ -41,42 +40,15 @@ class DataControl:
         return True
     
     # 모든 정보 불러오기
-    def getData(self, idx):
+    def getData(self):
         ret = "["
-        if math.ceil(len(os.listdir(self.directory))/self.listNum) < idx:
-            return self.failedRet
-        if os.path.exists(self.directory):
-            for filename in os.listdir(self.directory):
-                try:
-                    f = open(self.directory+"/"+filename, 'r')
-                    ret += f.read()+","
-                    f.close()
-                except IOError:
-                    return self.failedRet
-            ret = ret[0:len(ret)-1] + "]"
-        return ret
-
-    # 비밀번호 확인
-    def passwdCheck(self, now, name, passwd):
-        try:
-            f = open(self.getFilenameStr(now, name), 'r')
-            obj = json.load(f)
-            f.close()
-            if obj["password"] == passwd:
-                return True
-            else:
-                return False
-        except IOError:
-            return False
-    
-    # 데이터 수정
-    def changeData(self, data):
-        if self.passwdCheck(data["now"], data["name"], data["password"]):
-            filename = self.getFilenameStr(data["now"], data["name"])
-            if os.path.isfile(filename):
-                os.remove(filename)
-            else:
+        for filename in os.listdir(self.directory):
+            try:
+                f = open(self.directory+"/"+filename, 'r')
+                ret += f.read()+","
+                f.close()
+            except IOError:
                 return self.failedRet
-            return self.saveData(data)
-        else:
-            return self.failedRet
+        ret = ret[0:len(ret)-1] + "]"
+        return ret
+        
