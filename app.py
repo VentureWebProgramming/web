ab72cc8097641fe9594161c0ff25debc09b0be6b
JeonChangMin <wisechang1@naver.com>
2018-12-12 11:22:07 +0900

    Presentation 4

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.naver.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'wisechang1@naver.com'
-app.config['MAIL_PASSWORD'] = '**Same7Person7'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
@@ -53,7 +53,6 @@ def write(kind):
 @app.route("/review/data")
 def getData():
     data = Data["review"].getData()
-    print(data)
     return Response(response=data, status=200, mimetype='text/plain')
 
 @app.route("/review/delete", methods=['POST'])
